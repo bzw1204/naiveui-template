9df5135594c313a9b08ba929ac81f70a69803ef4
baizw <baizw@cnns.net>
2025-06-09 14:36:21 +0800

    feat: 新增仪表盘页面及相关元数据
    - 创建仪表盘页面`index.vue`，包含基本结构和标题
    - 新增仪表盘元数据`meta.ts`，定义页面标题、图标和排序信息

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({ name: 'Dashboard' })
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+  </div>
+</template>
---
+++ src/views/dashboard/meta.ts
@@ -0,0 +1,8 @@
+import IconHome from '~icons/carbon/Home'
+
+export default {
+  title: '首页',
+  icon: markRaw(IconHome),
+  order: 0,
+  isHome: true
+}
